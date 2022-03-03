3e034e764866a7ab3a7bb9fc3ff66d55829a4ad2
Théophile Stauder <theophilestauder@gmail.com>
2022-03-03 21:33:45 +0100

    Synchronize debug

--- Synchronizer.java
+++ Synchronizer.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.nio.file.*;
+import java.nio.file.StandardCopyOption;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Array;
 import java.util.*;
@@ -74,39 +75,12 @@ public class Synchronizer {
         Thread.sleep(1000);
         ArrayList <String> dirtyPaths1 = computeDirty(refCopy1, fs1, "");
         ArrayList <String> dirtyPaths2 = computeDirty(refCopy2, fs2, "");
-        reconcile(fs1, dirtyPaths1, fs2, dirtyPaths2, "");
-    }
+        System.out.println("Dirty 1 : "+ dirtyPaths1.toString());
+        System.out.println("Dirty 2 : "+ dirtyPaths2.toString());
 
-    /*
-    * A est le fileSystem 
-    * B est le fileSystem 
-    * p est le path 
-    recon(A,B,p) = 
-    1) if !dirty_A(p) && !dirty_B(p) Si les fichiers ne sont pas dirty on ne fait rien 
-            then (A,B)
-    2) else if isdir_A,B(p) 
-            then let {p1,p2,...,pn} = children_A,B(p) (in lexicographic order)
-            in let (A0,B0) = (A,B)
-                let (Ai+1, Bi+1) = recon(Ai,Bi,pi+1)
-                    for 0 <= i <= n
-                in (An,Bn)
-    3) else if !dirty_A(p) Si A n'est pas dirty mais B est dirty alors A prends B
-            then (A <-- B, B)
-    4) else if !dirty_B(p) Si B n'est pas dirty mais A est dirty alors B prends A
-            then (A,B <-- A)
-    5) else
-            (A,B)
-
-    Suppositions : 
-        Si la liste des dirtypaths est vide alors on ne fait rien vu que aucun fichier est modifié ou supprimé
-    */
-    public void reconcile(FileSystem fs1, ArrayList<String> dirtyPaths1, FileSystem fs2, ArrayList<String> dirtyPaths2,String currentRelativePath) throws IOException {
         if(dirtyPaths1.isEmpty() && dirtyPaths2.isEmpty()){
             // on ne fait rien, les fichiers et dossiers ne sont pas dirtys
             System.out.println("AUCUNE MODIFICATION");
-        }
-        else if(!dirtyPaths1.isEmpty() && !dirtyPaths2.isEmpty()){
-            System.out.println(" MODIFICATION PARTOUT");
         }
         else if(!dirtyPaths1.isEmpty()){
             for(String path : dirtyPaths1){
@@ -115,14 +89,33 @@ public class Synchronizer {
                 pathDest = pathDest.replace(fs1.getRoot(),fs2.getRoot());
                 File dest = new File(pathDest);
                 if(source.exists()){
-                    Files.copy(source.toPath(), dest.toPath() );
+                    if(source.isDirectory()){
+                        dest.mkdir();
+                    }else{
+                        Files.copy(source.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING );
+                    }
+
                 }else{
                     dest.delete();
                 }
             }
         }
         else if(!dirtyPaths2.isEmpty()){
-            System.out.println("modif UE SYN2");
+            for(String path : dirtyPaths2){
+                File source = new File(path);
+                String pathDest = path;
+                pathDest = pathDest.replace(fs2.getRoot(),fs1.getRoot());
+                File dest = new File(pathDest);
+                if(source.exists()){
+                    if(source.isDirectory()){
+                        dest.mkdir();
+                    }else{
+                        Files.copy(source.toPath(), dest.toPath() ,StandardCopyOption.REPLACE_EXISTING );
+                    }
+                }else{
+                    dest.delete();
+                }
+            }
         }
     }
 
